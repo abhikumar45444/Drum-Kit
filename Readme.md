8354f874897b8ac8f9022d6f2b08e8c2dfe75411
DINESH KUMAR <abhikumar45444@gmail.com>
2023-09-05 23:55:45 +0530

    initial commit, lets go!

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Drum Kit - fun interactive drumming
+# Description
+### Small project using HTML, CSS, JS combined to produce this interactive and fun drumming zone which uses mouse/touch events and keyboard events to play sounds...
+
+## 
+Visit Website: [Drum Kit Website]().
+
+## Language and Tools: 
+<div style="display: flex; justify-content: space-evenly">
+<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" alt="html" height="60" style="margin:4px">
+<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" alt="css" height="60" style="margin:4px">
+<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" alt="JavaScript" height="60" style="margin:4px">
+</div>
---
+++ index.js
@@ -0,0 +1,74 @@
+let drumButtons = document.querySelectorAll(".drum");
+
+let drumSounds = {
+    w:'./sounds/tom-1.mp3',
+    a:'./sounds/tom-2.mp3',
+    s:'./sounds/tom-3.mp3',
+    d:'./sounds/tom-4.mp3',
+    j:'./sounds/snare.mp3',
+    k:'./sounds/crash.mp3',
+    l:'./sounds/kick-bass.mp3',
+}
+
+drumButtons.forEach(button => {
+    button.addEventListener("click", function(){
+        
+        for(let eachSound in drumSounds)
+        {
+            if(button.innerHTML == eachSound)
+            {
+                console.log(drumSounds[eachSound]);
+                let mySound = new Audio(drumSounds[eachSound]);
+                mySound.currentTime = 0;
+                mySound.play();
+            }
+        }
+    })
+})
+
+
+document.addEventListener("keydown", function(event){
+    
+    if(event.key == "w")
+    {
+        let mySound = new Audio(drumSounds[event.key]);
+        mySound.currentTime = 0;
+        mySound.play();
+    }
+    else if(event.key == "a")
+    {
+        let mySound = new Audio(drumSounds[event.key]);
+        mySound.currentTime = 0;
+        mySound.play();
+    }
+    else if(event.key == "s")
+    {
+        let mySound = new Audio(drumSounds[event.key]);
+        mySound.currentTime = 0;
+        mySound.play();
+    }
+    else if(event.key == "d")
+    {
+        let mySound = new Audio(drumSounds[event.key]);
+        mySound.currentTime = 0;
+        mySound.play();
+    }
+    else if(event.key == "j")
+    {
+        let mySound = new Audio(drumSounds[event.key]);
+        mySound.currentTime = 0;
+        mySound.play();
+    }
+    else if(event.key == "k")
+    {
+        let mySound = new Audio(drumSounds[event.key]);
+        mySound.currentTime = 0;
+        mySound.play();
+    }
+    else if(event.key == "l")
+    {
+        let mySound = new Audio(drumSounds[event.key]);
+        mySound.currentTime = 0;
+        mySound.play();
+    }
+})
